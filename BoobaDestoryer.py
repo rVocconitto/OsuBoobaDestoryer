cb6bda628c1e3d734a0c08cb42fc6784bc2d6ce7
Voccon <133379410+rVocconitto@users.noreply.github.com>
2024-04-27 12:27:59 +0200

    Source
    Source

---
+++ BoobaDestoryer.py
@@ -0,0 +1,12 @@
+import os
+
+def delete_jpg_files_in_folders(root_folder):
+    for folder_path, _, files in os.walk(root_folder):
+        for file in files:
+            if file.lower().endswith('.jpg') or file.lower().endswith('.png'):
+                file_path = os.path.join(folder_path, file)
+                os.remove(file_path)
+                print(f"Anime Booba be gone sigmas mindset or smth idk: {file_path}")
+root_folder = input("Enter the songs folder path: ")
+
+delete_jpg_files_in_folders(root_folder)
